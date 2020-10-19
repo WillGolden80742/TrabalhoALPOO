0993ccaec587bf5382db66ab533c19711a5c813c
williamgolden80742 <52113843+williamgolden80742@users.noreply.github.com>
2020-10-19 11:24:34 -0300

    adicionando setters e getters
    adicionando setters e getters

--- src/Model/DAO/professoresDAO.java
+++ src/Model/DAO/professoresDAO.java
@@ -10,5 +10,42 @@ package Model.DAO;
  * @author William
  */
 public class professoresDAO {
+    private int IdentProf;
+    private String NomeProf;
+    private String DataNasc;
+    private String EspecProf;
+
+    public int getIdentProf() {
+        return IdentProf;
+    }
+
+    public void setIdentProf(int IdentProf) {
+        this.IdentProf = IdentProf;
+    }
+
+    public String getNomeProf() {
+        return NomeProf;
+    }
+
+    public void setNomeProf(String NomeProf) {
+        this.NomeProf = NomeProf;
+    }
+
+    public String getDataNasc() {
+        return DataNasc;
+    }
+
+    public void setDataNasc(String DataNasc) {
+        this.DataNasc = DataNasc;
+    }
+
+    public String getEspecProf() {
+        return EspecProf;
+    }
+
+    public void setEspecProf(String EspecProf) {
+        this.EspecProf = EspecProf;
+    }
+    
     
 }
